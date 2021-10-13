88f492d02a641637add5b72db6884acd30fd7591
Mika Nokka <mika.nokka@unikie.com>
2021-10-13 10:07:25 +0300

    Initial commit

---
+++ turtlesim_launcher.py
@@ -0,0 +1,36 @@
+#
+# ROS2 example: https://docs.ros.org/en/rolling/Tutorials/Launch-Files/Creating-Launch-Files.html
+#
+# Requires ROS2 and turtlesim being installed to system
+# launnch: ros2 launch turtlesim_launcher.py 
+#
+# send commands to sim1: ros2 topic pub -r 1 /turtlesim1/turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: -1.8}}"
+# sim2 should follow the sim1 rotation
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            namespace='turtlesim1',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='turtlesim',
+            namespace='turtlesim2',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='turtlesim',
+            executable='mimic',
+            name='mimic',
+            remappings=[
+                ('/input/pose', '/turtlesim1/turtle1/pose'),
+                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
+            ]
+        )
+    ])
